df0973d72d6f7023df4e10326f5fb53811f3e1aa
Y.Takumi <130421725+Takumi1217@users.noreply.github.com>
2024-11-17 11:50:14 +0000

    static pages add

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,27 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  def agreement
+  end
+
+  def policy
+  end
+
   def help
+  end
+
+  def contact
+  end
+
+  def search
+  end
+
+  def bookmark
+  end
+
+  def notice
+  end
+
+  def history
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get 'static_pages/search'
+  get 'static_pages/bookmark'
+  get 'static_pages/notice'
+  get 'static_pages/history'
+  get 'static_pages/agreement'
+  get 'static_pages/policy'
   get 'static_pages/help'
+  get 'static_pages/contact'
   get 'users/new'
 end
