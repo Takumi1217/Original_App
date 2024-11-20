ae990700a07aeaf0c688b29310f2666e450bb01c
Y.Takumi <130421725+Takumi1217@users.noreply.github.com>
2024-11-20 10:44:44 +0000

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_16_015700) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_20_104148) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
